60d35a0f49797445105ee53b134a53e09a075cb1
GUaN <152492391+GU7432@users.noreply.github.com>
2025-12-16 17:50:54 +0800

    增加我的基本設定

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,33 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+function my_paste(reg)
+  return function(lines)
+    local content = vim.fn.getreg('"')
+    return vim.split(content, "\n")
+  end
+end
+
+if os.getenv("SSH_TTY") == nil then
+  vim.opt.clipboard:append("unnamedplus")
+else
+  vim.opt.clipboard:append("unnamedplus")
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      --[ 小括号里面的内容可能是毫无意义的，但是保持原样可能看起来更好一点 ]
+      ["+"] = my_paste("+"),
+      ["*"] = my_paste("*"),
+    },
+  }
+end
+
+vim.opt.expandtab = true -- 按 Tab 輸入空白，而不是 \t
+vim.g.autoformat = false -- 關掉自動格式化
+
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
