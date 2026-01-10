499aa55569c50744a72beaeb1ac87ad025b8a4b3
GUaN <152492391+GU7432@users.noreply.github.com>
2026-01-11 07:00:10 +0800

    init

---
+++ lua/core/basic.lua
@@ -0,0 +1,12 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.cursorline = true
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.autoread = true
+
+vim.g.autoformat = false -- 關掉自動格式化
